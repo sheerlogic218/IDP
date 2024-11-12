1e8a135dfc037830ee2b67f6a067fc8b9d941841
praewataylor <pt462@cam.ac.uk>
2024-11-12 10:46:02 +0000

    Ultrasonic_sensor_test

---
+++ Ultrasonic_sensor_test.ino
@@ -0,0 +1,21 @@
+/*
+URM09 Ultrasonic Sensor test
+*/
+#define MAX_RANG (520)//the max measurement value of the module is 520cm(a little bit longer than effective max range)
+#define ADC_SOLUTION (1023.0)//ADC accuracy of Arduino UNO is 10bit
+int sensityPin = A0; // select the input pin
+void setup() {
+// Serial init
+Serial.begin(9600);
+}
+float dist_t, sensity_t;
+void loop() {
+  // read the value from the sensor:
+  sensity_t = analogRead(sensityPin);
+  // turn the ledPin on
+  dist_t = sensity_t * MAX_RANG / ADC_SOLUTION;//
+  Serial.print(dist_t,0);
+  Serial.println("cm");
+  delay(500);
+}
+
