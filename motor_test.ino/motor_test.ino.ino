32c3c3593d919579de1856b422ca87eec7cbf1aa
Bailen O'Farrell <59749194+sheerlogic218@users.noreply.github.com>
2024-11-14 13:07:49 +0000

    tuned turn functions and move functions
    now can follow line well and takes corners like a champ

--- motor_test.ino/motor_test.ino.ino
+++ motor_test.ino/motor_test.ino.ino
@@ -1,34 +1,30 @@
 #include "IDP_lib.h"
 
-volatile byte state = LOW;
+// volatile byte state = LOW;
 
 
 void junction(){
     if (fls_state == 1 && frs_state == 0){
-        main_motors.change_MR_speed(40);
-        main_motors.go_forward();
-        delay(1000);
+        // main_motors.change_MR_speed(40);
+        // main_motors.go_forward();
+        // delay(1000);
+        main_motors.turn_90_left();
     }
     else if (fls_state == 0 && frs_state == 1) {
-        main_motors.change_ML_speed(40);
-        main_motors.go_forward();
-        delay(1000);
+        // main_motors.change_ML_speed(40);
+        // main_motors.go_forward();
+        // delay(1000);
+        main_motors.turn_90_right();
     }
     else if (fls_state == 1 && frs_state == 1) {
-        main_motors.change_MR_speed(20);
-        main_motors.go_forward();
-        delay(2000);
+        // main_motors.change_MR_speed(20);
+        // main_motors.go_forward();
+        // delay(2000);
+        main_motors.turn_90_left();
     }
 }
 
 
-
-int calc_dist(int speed, int time) {
-    main_motors.set_speed(speed);
-    main_motors.go_forward();
-    delay(time);
-
-}
 
 void junction2(){
     main_motors.stop();
@@ -36,71 +32,80 @@ void junction2(){
     delay(200);
 }
 
-void interrupt_function(){
-    volatile static unsigned long last_interrupt = 0;
-    unsigned long t = millis();
-    if (t - last_interrupt > 500UL){
-      state = !state;
-    }
-    last_interrupt = t;
-    //Serial.println(state);
-  }
+// void interrupt_function(){
+//     volatile static unsigned long last_interrupt = 0;
+//     unsigned long t = millis();
+//     if (t - last_interrupt > 500UL){
+//       state = !state;
+//       main_motors.stop();
+//     }
+//     last_interrupt = t;
+//     //Serial.println(state);
+//   }
 
-void setup() {
-    pinMode(3,INPUT);
-    while ( !digitalRead(3) );
-    Serial.begin(9600);
-    Serial.println("a");
-    attachInterrupt(digitalPinToInterrupt(3), interrupt_function, RISING);
-    //while(1);
-    // AFMS.begin();
-    // main_motors.set_speed(0);
-    // pinMode(left_sensor, INPUT);
-    // //pinMode(center_sensor, INPUT);
-    // pinMode(right_sensor, INPUT);
-    // pinMode(far_left_sensor,INPUT);
-    // pinMode(far_right_sensor, INPUT);
+// void setup() {
+//     pinMode(3,INPUT);
+//     while ( !digitalRead(3) );
+//     Serial.begin(9600);
+//     Serial.println("a");
+//     attachInterrupt(digitalPinToInterrupt(3), interrupt_function, RISING);
+//     AFMS.begin();
+//     main_motors.set_speed(0);
+//     pinMode(left_sensor, INPUT);
+//     //pinMode(center_sensor, INPUT);
+//     pinMode(right_sensor, INPUT);
+//     pinMode(far_left_sensor,INPUT);
+//     pinMode(far_right_sensor, INPUT);
     
-}
+// }
 
 void loop(){
   //Serial.println(state);
   if (state) {
     Serial.println("we are running");
+    // main_motors.set_speed(255);
+    // main_motors.go_forward();
+    //main_motors.move_forward(100);
+    
+    // main_motors.turn_90_left();
+    // delay(3000);
+    //Serial.println(main_motors.max_wheel_speed);
+    read_sensors();
+    //test code for 4 sensor following
+    if (ls_state == 1 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
+        main_motors.set_speed(200);
+        main_motors.go_forward();
+        Serial.println("on line");
     }
+    else if (ls_state == 1 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
+        main_motors.change_MR_speed(10);
+        main_motors.go_forward();
+        Serial.println("right of line");
+    }
+    
+    else if (ls_state == 0 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
+        main_motors.change_ML_speed(10);
+        main_motors.go_forward();
+        Serial.println("left of line");
+    }
+    //something gone wrong
+    else if (ls_state == 0 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
+        main_motors.stop();
+        Serial.println("something wrong");
+    }
+    //junction logic
+    else if(fls_state == 1 || frs_state == 1){
+        Serial.println("at junction");
+        main_motors.move_forward(30);
+        junction();
+    }
+    //delay(100);
+  }
+
   else {
     Serial.println("we are not running");
+    main_motors.stop();
   }
-    //read_sensors();
-    //test code for 4 sensor following
-    // if (ls_state == 1 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
-    //     main_motors.set_speed(200);
-    //     main_motors.go_forward();
-    //     Serial.println("on line");
-    // }
-    // else if (ls_state == 1 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
-    //     main_motors.change_MR_speed(10);
-    //     main_motors.go_forward();
-    //     Serial.println("right of line");
-    // }
-    
-    // else if (ls_state == 0 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
-    //     main_motors.change_ML_speed(10);
-    //     main_motors.go_forward();
-    //     Serial.println("left of line");
-    // }
-    // //something gone wrong
-    // else if (ls_state == 0 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
-    //     main_motors.set_speed(50);
-    //     main_motors.go_backward();
-    //     Serial.println("something wrong");
-    // }
-    // //junction logic
-    // else {
-    //     Serial.println("at junction");
-    //     junction();
-    // }
-
 }
 
 
