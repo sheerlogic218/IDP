4bce70e81a2bfb75da81181330428052c59d7805
Leo Hammett <leo@hammett.biz>
2024-11-20 10:43:20 +0000

    Updated navigation lets hope

--- System Routine/system.ino
+++ System Routine/system.ino
@@ -1,26 +1,31 @@
 #include "IDP_lib.h"
 
-// Constants for movement directions
+// Constants for turn directions
 const int STRAIGHT_ON = 0;
 const int RIGHT = 1;
 const int FULL_AROUND = 2;
 const int LEFT = 3;
 const int SPECIAL_FROM_THE_LEFT = 4;
 const int SPECIAL_FROM_THE_RIGHT = 5;
-const int FORWARD_THEN_BACKWARD = 11;
-const int REVERSE = 8;
 const int STOP = 7;
+const int RIGHT_DIP = 8;
+const int LEFT_DIP = 9;
+
+// constants for move directions
+const int FORWARD = 1;
+const int FORWARD_UNTIL_END_THEN_START_REVERSE = 2;
+const int REVERSE = 3;
 
 // Path array defining the robot's route
 int path[24] = {
-    SPECIAL_FROM_THE_LEFT, LEFT, // BLICK!
+    SPECIAL_FROM_THE_LEFT, LEFT,                        // BLOCK
     RIGHT, STRAIGHT_ON, RIGHT, SPECIAL_FROM_THE_LEFT,   // END SPECIAL AT MIDDLE BLOCK
     RIGHT, RIGHT, RIGHT, SPECIAL_FROM_THE_LEFT,         // END SPECIAL AT MIDDLE BLOCK
     LEFT, LEFT, LEFT, SPECIAL_FROM_THE_RIGHT,           // END SPECIAL AT MIDDLE BLOCK
-    RIGHT, LEFT, LEFT, STRAIGHT_ON,                     // BLICK
+    RIGHT, LEFT, LEFT, STRAIGHT_ON,                     // BLOCK
     LEFT, STRAIGHT_ON, LEFT, SPECIAL_FROM_THE_RIGHT,    // END SPECIAL AT MIDDLE BLOCK
-    LEFT,                                // There's a hidden block on this road
-    STOP // Use comments to explain each part of the route.
+    LEFT,                                               // There's a hidden block on this road
+    STOP                                                // Use comments to explain each part of the route.
 };
 
 int progress = 0;
@@ -28,14 +33,15 @@ int special_mode = -1;
 int special_progress = 0;
 int direction = 0;
 int is_magnet = 0; // Magnetic is recyclable
+int move_mode = 1;
 
 // Special paths for different modes
 int special_path[5][6] = {
-    {STRAIGHT_ON, RIGHT, FORWARD_THEN_BACKWARD, RIGHT, LEFT, STRAIGHT_ON},
-    {RIGHT, RIGHT, FORWARD_THEN_BACKWARD, LEFT, STOP, STOP}, // RLEFT goes forward after
-    {LEFT, FORWARD_THEN_BACKWARD, RIGHT, LEFT, STRAIGHT_ON, STOP},
-    {STRAIGHT_ON, LEFT, RIGHT, FORWARD_THEN_BACKWARD, LEFT, STOP},
-    {LEFT, LEFT, FORWARD_THEN_BACKWARD, STOP, STOP, STOP},
+    {STRAIGHT_ON, RIGHT_DIP, RIGHT, LEFT, STRAIGHT_ON},
+    {RIGHT, RIGHT_DIP, LEFT, STOP, STOP}, // RLEFT goes forward after
+    {LEFT_DIP, RIGHT, LEFT, STRAIGHT_ON, STOP},
+    {STRAIGHT_ON, LEFT, RIGHT_DIP, LEFT, STOP},
+    {LEFT, LEFT_DIP, STOP, STOP, STOP},
 };
 
 // Junction function to handle the robot's movements at junctions
@@ -45,7 +51,7 @@ void turn_junction() {
     switch (turn_direction) {
         case STRAIGHT_ON:
             // Code to go straight
-            delay(1000);
+            main_motors.move_forward(30);
             break;
         case RIGHT:
             // Code to turn right
@@ -54,6 +60,16 @@ void turn_junction() {
         case LEFT:
             // Code to turn left
             main_motors.turn_90_left();
+            break;
+        case RIGHT_DIP:
+            // Code to turn right
+            main_motors.turn_90_right();
+            move_mode = FORWARD_UNTIL_END_THEN_START_REVERSE;
+            break;
+        case LEFT_DIP:
+            // Code to turn left
+            main_motors.turn_90_left();
+            move_mode = FORWARD_UNTIL_END_THEN_START_REVERSE;
             break;
         case SPECIAL_FROM_THE_LEFT:
             // Code for special action to the left
@@ -70,41 +86,74 @@ void turn_junction() {
     }
 }
 
-// Function to handle special junction movements
-void special_junction() {
-    direction = special_path[special_mode][special_progress];
-    special_progress++;
-    switch (direction) {
-        case STRAIGHT_ON:
-            // Code to go straight
-            break;
-        case RIGHT:
-            // Code to turn right
-            main_motors.turn_90_right();
-            break;
-        case LEFT:
-            // Code to turn left
-            Serial.println("Special junction: Turning left.");
-            main_motors.turn_90_left();
-            break;
-        case REVERSE:
-            Serial.println("Special junction: Reversing.");
-            main_motors.go_backward();
-            while (fls_state == 0 && frs_state == 0) {    // Add alignment
-                Serial.println("Reversing until front sensors detect line.");
-                delay(50);
-            }
-            break;
-        case STOP:
-            Serial.println("Special junction: Stopping special mode.");
-            special_mode = -1;
-            special_progress = 0;
-            break;
-        default:
-            Serial.println("Special junction: Invalid direction.");
-            break;
+
+
+void loop() {
+    if (state) {
+        Serial.println("System is running.");
+        system_decisions();
+    } else {
+        Serial.println("System is not running.");
+        main_motors.stop();
     }
 }
+
+// Function for making system decisions based on sensor readings
+void system_decisions() {
+    do_a_move();
+    Serial.println("Executing system decisions.");
+    // Junction logic
+    if (fls_state == 1 || frs_state == 1) {
+        Serial.println("At junction.");
+        turn_junction(get_turn_direction());
+    }
+}
+
+int get_turn_direction()
+{
+    if(special_mode == -1)
+    {
+        int turn_direction = path[progress];
+        progress++;
+    }
+    else
+    {
+        direction = special_path[special_mode][special_progress];
+        special_progress++;
+        if(direction == STOP)
+        {
+            special_mode = -1;
+            special_progress = 0;
+        }
+    }
+}
+
+void do_a_move()
+{
+    if(move_direction == FORWARD)
+    {
+        line_track_forward();
+    }
+    if(move_direction == FORWARD_UNTIL_END_THEN_START_REVERSE)
+    {
+        line_track_backward();
+        if(ls_state == 0 && rs_state == 0)
+        {
+            move_direction == REVERSE;
+        }
+    }
+    if(move_direction == REVERSE)
+    {
+        line_track_backward();
+        if(fls_state == 1 || frs_state == 1)
+        {
+            move_direction = FORWARD;
+        }
+    }
+
+}
+
+
 
 // Function for line tracking forward
 void line_track_forward() {
@@ -152,43 +201,3 @@ void line_track_backward() {
         Serial.println("Line tracking backward: Lost line, adjusting speed.");
     }
 }
-
-void loop() {
-    if (state) {
-        Serial.println("System is running.");
-        system_decisions();
-    } else {
-        Serial.println("System is not running.");
-        main_motors.stop();
-    }
-}
-
-// Function for making system decisions based on sensor readings
-void system_decisions() {
-    line_track_forward();
-    Serial.println("Executing system decisions.");
-    // Junction logic
-    if (fls_state == 1 || frs_state == 1) {
-        Serial.println("At junction.");
-        if (special_mode == -1) {
-            junction();
-        } else {
-            special_junction();
-        }
-    }
-}
-
-int path_switching()
-{
-    if(special_mode == -1)
-    {
-        int turn_direction = path[progress];
-        progress++;
-    }
-    else
-    {
-
-    }
-}
-
-
