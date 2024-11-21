1db9db689dc9d22c4a3b5f02f47f1f41f624104c
Bailen O'Farrell <59749194+sheerlogic218@users.noreply.github.com>
2024-11-21 21:13:09 +0000

    few things to get it running

--- BAILEN_test_area/system_IPD.cpp
+++ BAILEN_test_area/system_IPD.cpp
@@ -1,4 +1,9 @@
-#include "IDP_lib.h"
+// testing_area.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "test.h"
+using namespace std;
 
 // Constants for turn directions
 const int STRAIGHT_ON = 0;
@@ -30,7 +35,7 @@ const int REVERSE_MOVE = 3;
 int path[1] = {
   LEFT
 };
-int path_length = sizeof(path)/sizeof(path[0]);
+int path_length = sizeof(path) / sizeof(path[0]);
 
 
 int progress = 0;
@@ -51,11 +56,11 @@ int special_path[5][6] = {
 
 
 int get_turn_direction() {
-    if (special_mode == -1  &&  progress <= path_length ) {
+    if (special_mode == -1 && progress <= path_length) {
         int turn_direction = path[progress];
         //leds.blue_on();
         progress++;
-    } else{
+    } else {
         direction = special_path[special_mode][special_progress];
         //leds.red_on();
         special_progress++;
@@ -64,6 +69,7 @@ int get_turn_direction() {
             special_progress = 0;
         }
     }
+    return direction;
 }
 
 
@@ -72,50 +78,50 @@ void turn_junction(int turn_direction) {
     cout << "Junction turn_direction: ";
     cout << turn_direction << endl;
     switch (turn_direction) {
-        case STRAIGHT_ON:
-            // Code to go straight
-            //leds.blue_blink();
-            main_motors.move_forward(30);
-            break;
-        case RIGHT:
-            //leds.red_blink();
-            // Code to turn right
-            main_motors.turn_90_right();
-            break;
-        case LEFT:
-            //leds.blue_blink();
-            //leds.red_blink();
-            // Code to turn left
-            main_motors.turn_90_left();
-            break;
-        case RIGHT_DIP:
-            //leds.red_blink();
-            //leds.red_blink();
-            // Code to turn right
-            main_motors.turn_90_right();
-            move_mode = FORWARD_UNTIL_END_THEN_START_REVERSE;
-            break;
-        case LEFT_DIP:
-            //leds.blue_blink();
-            //leds.red_blink();
-            // Code to turn left
-            main_motors.turn_90_left();
-            move_mode = FORWARD_UNTIL_END_THEN_START_REVERSE;
-            break;
-        case SPECIAL_FROM_THE_LEFT:
-            // Code for special action to the left
-            special_mode = 0 + is_magnet;
-            turn_junction(get_turn_direction());
-            break;
-        case SPECIAL_FROM_THE_RIGHT:
-            // Code for special action to the left
-            special_mode = 2 + is_magnet;
-            turn_junction(get_turn_direction());
-            break;
-        default:
-            // Default action for invalid direction
-            main_motors.stop();
-            break;
+    case STRAIGHT_ON:
+        // Code to go straight
+        //leds.blue_blink();
+        main_motors.move_forward(30);
+        break;
+    case RIGHT:
+        //leds.red_blink();
+        // Code to turn right
+        main_motors.turn_90_right();
+        break;
+    case LEFT:
+        //leds.blue_blink();
+        //leds.red_blink();
+        // Code to turn left
+        main_motors.turn_90_left();
+        break;
+    case RIGHT_DIP:
+        //leds.red_blink();
+        //leds.red_blink();
+        // Code to turn right
+        main_motors.turn_90_right();
+        move_mode = FORWARD_UNTIL_END_THEN_START_REVERSE;
+        break;
+    case LEFT_DIP:
+        //leds.blue_blink();
+        //leds.red_blink();
+        // Code to turn left
+        main_motors.turn_90_left();
+        move_mode = FORWARD_UNTIL_END_THEN_START_REVERSE;
+        break;
+    case SPECIAL_FROM_THE_LEFT:
+        // Code for special action to the left
+        special_mode = 0 + is_magnet;
+        turn_junction(get_turn_direction());
+        break;
+    case SPECIAL_FROM_THE_RIGHT:
+        // Code for special action to the left
+        special_mode = 2 + is_magnet;
+        turn_junction(get_turn_direction());
+        break;
+    default:
+        // Default action for invalid direction
+        main_motors.stop();
+        break;
     }
 }
 
@@ -127,20 +133,20 @@ void line_track_forward() {
     if (ls_state == 1 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
         main_motors.set_speed(230);
         main_motors.go_forward();
-        cout << "Line tracking forward: On line."<< endl;
+        cout << "Line tracking forward: On line." << endl;
     } else if (ls_state == 1 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
         main_motors.change_MR_speed(12);
         main_motors.go_forward();
-        cout << "Line tracking forward: Right of line."<< endl;
+        cout << "Line tracking forward: Right of line." << endl;
     } else if (ls_state == 0 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
         main_motors.change_ML_speed(12);
         main_motors.go_forward();
-        cout << "Line tracking forward: Left of line."<< endl;
+        cout << "Line tracking forward: Left of line." << endl;
     } else if (ls_state == 0 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
         // Something went wrong
         //main_motors.stop();
         main_motors.move_backward(10);
-        cout << "Line tracking forward: Lost line, stopping."<< endl;
+        cout << "Line tracking forward: Lost line, stopping." << endl;
     }
 }
 
@@ -151,26 +157,26 @@ void line_track_backward() {
     if (ls_state == 1 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
         main_motors.set_speed(230);
         main_motors.go_backward();
-        cout << "Line tracking backward: On line."<< endl;
+        cout << "Line tracking backward: On line." << endl;
     } else if (ls_state == 0 && rs_state == 1 && fls_state == 0 && frs_state == 0) {
         main_motors.change_MR_speed(-10);
         main_motors.go_backward();
-        cout << "Line tracking backward: Right of line."< endl;
+        cout << "Line tracking backward: Right of line." << endl;
     } else if (ls_state == 1 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
         main_motors.change_ML_speed(-10);
         main_motors.go_backward();
-        cout << "Line tracking backward: Left of line."<< endl;
+        cout << "Line tracking backward: Left of line." << endl;
     } else if (ls_state == 0 && rs_state == 0 && fls_state == 0 && frs_state == 0) {
         // Something went wrong
         // main_motors.set_speed(130);
         // main_motors.go_backward();
         main_motors.move_forward(30);
-        cout << "Line tracking backward: Lost line, adjusting speed."<< endl;
+        cout << "Line tracking backward: Lost line, adjusting speed." << endl;
     }
 }
 
 void do_a_move() {
-    if(move_mode == FORWARD_MOVE){
+    if (move_mode == FORWARD_MOVE) {
         line_track_forward();
     } if (move_mode == FORWARD_UNTIL_END_THEN_START_REVERSE) {
         line_track_backward();
@@ -188,20 +194,23 @@ void do_a_move() {
 // Function for making system decisions based on sensor readings
 void system_decisions() {
     do_a_move();
-    cout << "Executing system decisions."<< endl;
+    cout << "Executing system decisions." << endl;
     // Junction logic
     if (fls_state == 1 || frs_state == 1) {
-        cout << "At junction."<< endl;
+        cout << "At junction." << endl;
         turn_junction(get_turn_direction());
     }
 }
 
 void loop() {
-    if (state) {
-        //cout <<"System is running."<< endl;
         system_decisions();
-    } else {
-        //cout <<"System is not running."<< endl;
-        main_motors.stop();
+}
+
+
+
+int main() {
+    while (1) {
+        sleep_for(seconds(1));
+        loop();
     }
-}0
+}
